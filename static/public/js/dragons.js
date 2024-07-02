94980fd8f17b3399234bcf87a1b63729d0320384
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2024-07-02 19:06:19 +0200

    Cambios hechos y subida de imagenes

--- static/public/js/dragons.js
+++ static/public/js/dragons.js
@@ -27,15 +27,6 @@ fetch("https://apidragonball.vercel.app/dragons")
                     <a href="https://apidragonball.vercel.app/dragons/${dato.id}">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${dato.name}</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        Genero: ${dato.genre}
-                    </p>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        Raza: ${dato.race}
-                    </p>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        Planeta: ${dato.planet}
-                    </p>
                 </div>
             `;
 
