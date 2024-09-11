5b717e273ad706bfbdfc6cff6028e821acae5084
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2024-09-11 22:48:03 +0200

    Minified All

--- static/public/js/dragons.js
+++ static/public/js/dragons.js
@@ -1,40 +1,12 @@
-fetch("https://www.dragonballapi.com/dragons")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('La respuesta de la red no fue válida');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data); // Aquí puedes manejar los datos de la respuesta
-
-
-    const content = document.querySelector("#content");
-
-        // Itera sobre los datos y crea elementos HTML
-        data.forEach((dato) => {
-            const div = document.createElement("div");
-            div.className = "max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";
-
-            // Crea el contenido del elemento
-            div.innerHTML = `
-                <div class="bg-white">
-                    <a href="https://www.dragonballapi.com/dragons/${dato.id}">
-                        <img class="rounded-t-lg" src="${dato.image}" alt="" />
-                    </a>
-                </div>
-                <div class="p-5">
-                    <a href="https://www.dragonballapi.com/dragons/${dato.id}">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${dato.name}</h5>
-                    </a>
-                </div>
-            `;
-
-            // Agrega el elemento al contenedor
-            content.appendChild(div);
-        });
-
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+fetch("https://www.dragonballapi.com/dragons").then(response=>{if(!response.ok){throw new Error('La respuesta de la red no fue válida')}return response.json()}).then(data=>{console.log(data);const content=document.querySelector("#content");data.forEach((dato)=>{const div=document.createElement("div");div.className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";div.innerHTML=`
+  <div class="bg-white">
+      <a href="https://www.dragonballapi.com/dragons/${dato.id }">
+          <img class="rounded-t-lg" src="${dato.image }" alt="" />
+      </a>
+  </div>
+  <div class="p-5">
+      <a href="https://www.dragonballapi.com/dragons/${dato.id }">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${dato.name }</h5>
+      </a>
+  </div>
+`;content.appendChild(div)})}).catch(error=>{console.error('Error:',error)});
--- static/public/js/script.js
+++ static/public/js/script.js
@@ -1,49 +1,21 @@
-fetch("https://www.dragonballapi.com/dragonballz")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('La respuesta de la red no fue válida');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data); // Aquí puedes manejar los datos de la respuesta
-
-
-    const contenedor = document.querySelector("#contenedor");
-
-        // Itera sobre los datos y crea elementos HTML
-        data.forEach((dato) => {
-            const div = document.createElement("div");
-            div.className = "max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";
-
-            // Crea el contenido del elemento
-            div.innerHTML = `
-                <div class="bg-white">
-                    <a href="https://www.dragonballapi.com/dragonballz/${dato.id}">
-                        <img class="rounded-t-lg" src="${dato.image}" alt="" />
-                    </a>
-                </div>
-                <div class="p-5">
-                    <a href="https://www.dragonballapi.com/dragonballz/${dato.id}">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${dato.name}</h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        Genero: ${dato.genre}
-                    </p>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        Raza: ${dato.race}
-                    </p>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        Planeta: ${dato.planet}
-                    </p>
-                </div>
-            `;
-
-            // Agrega el elemento al contenedor
-            contenedor.appendChild(div);
-        });
-
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+fetch("https://www.dragonballapi.com/dragonballz").then(response=>{if(!response.ok){throw new Error('La respuesta de la red no fue válida')}return response.json()}).then(data=>{console.log(data);const contenedor=document.querySelector("#contenedor");data.forEach((dato)=>{const div=document.createElement("div");div.className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";div.innerHTML=`
+    <div class="bg-white">
+        <a href="https://www.dragonballapi.com/dragonballz/${dato.id }">
+            <img class="rounded-t-lg" src="${dato.image }" alt="" />
+        </a>
+    </div>
+    <div class="p-5">
+        <a href="https://www.dragonballapi.com/dragonballz/${dato.id }">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${dato.name }</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Genero: ${dato.genre }
+        </p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Raza: ${dato.race }
+        </p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Planeta: ${dato.planet }
+        </p>
+    </div>
+`;contenedor.appendChild(div)})}).catch(error=>{console.error('Error:',error)});
